6c27cdba2e784e5e6273c0ffc0ac9950b5c5ca94
ranggatw <rangga.public@gmail.com>
2022-11-25 22:48:38 +0700

    init

---
+++ src/typeDasar.ts
@@ -0,0 +1,16 @@
+// Tipe Data String
+let nama: string = "rangga";
+nama = "string";
+
+// Tipe Data Number
+let umur: number = 25;
+// umur = "dua lima" >> error karena bukan number
+// umur = { asds: "sdsdsd" }; // >> error karena bukan number
+// umur = [1, 2, 3, 4]; // >> error karena bukan number
+umur = 30; // tidak error
+
+// Tipe data Boolean
+let isMarried: boolean;
+
+isMarried = true;
+isMarried = false;
