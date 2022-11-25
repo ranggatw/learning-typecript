2be05efcd5b9e70ec18d8aebe8f3d6e60d4fa4d7
ranggatw <rangga.public@gmail.com>
2022-11-25 23:09:55 +0700

    tipe data array

---
+++ src/array.ts
@@ -0,0 +1,12 @@
+// Array
+let array: number[]; // array yang di dalamnya harus berupa number
+
+array = [1, 2, 3];
+
+let array2: string[]; // array yang di dalamnya harus berupa string
+
+array2 = ["string 1", "string 2"];
+
+let array3: any[]; // array dengan banyak tipe data
+
+array3 = [1, "string", true, {}];
