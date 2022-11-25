0fa52829dbf7ec97156391da4ed8969a7984082a
ranggatw <rangga.public@gmail.com>
2022-11-25 23:39:58 +0700

    tipe data array tuples

--- src/array.ts
+++ src/array.ts
@@ -10,3 +10,9 @@ array2 = ["string 1", "string 2"];
 let array3: any[]; // array dengan banyak tipe data
 
 array3 = [1, "string", true, {}];
+
+// Tupples >> sebuah array yang isinya bisa berbagai tipe data tetapi nilai / itemsnya terbatas
+let biodata: [string, number];
+// biodata = ["Yogyakarta", true]; // akan error karena array ke dua harus beruapa number bukan boolean
+// biodata = ["Yogyakarta", 123, false]; // akan error karena array hanya boleh meneriam 2 items saja
+biodata = ["Yogyakarta", 123];
