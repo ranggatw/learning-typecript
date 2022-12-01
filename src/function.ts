0f1cc5bd18d796759b090e89e093684d278e3a32
ranggatw <rangga.public@gmail.com>
2022-12-01 14:49:58 +0700

    Tipe data pada function

--- src/function.ts
+++ src/function.ts
@@ -14,9 +14,20 @@ function getAge(): number {
   return 1212;
 }
 
-function printName(): void { // >> dengan mendeclare menjadi void maka function tidak boleh mengembalikan nilai pada return
-  console.log("console dari print name");
-  // return "Print" // akan error 
+function printName(): void {
+  // >> dengan mendeclare menjadi void maka function tidak boleh mengembalikan nilai pada return
+  // console.log("console dari print name");
+  // return "Print" // akan error
 }
 
 printName();
+
+// Argument Types pada function
+
+function multiply(val1: number, val2: number): number {
+  return val1 * val2;
+}
+
+const res = multiply(20, 10); // arguments harus number
+console.log(res);
+
